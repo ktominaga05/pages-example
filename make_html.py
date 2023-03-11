f14140b9785f3bbed84843c2273be254a94e8088
ktominaga05 <64580018+ktominaga05@users.noreply.github.com>
2023-03-11 15:25:28 +0000

    fixed

--- make_html.py
+++ make_html.py
@@ -1,5 +1,8 @@
+from pathlib import Path
 import plotly.express as px
+import os
 
+os.makedirs('output', exist_ok=True)
 df = px.data.iris() # iris is a pandas DataFrame
 fig = px.scatter(df, x="sepal_width", y="sepal_length")
-fig.write_html('index.html')
+fig.write_html('output/index.html')
